d7d465775bff7e65637ad1ceb2c9f4c4d5d69535
Mikita.Shah <mikitashah2009@gmail.com>
2024-10-31 18:05:25 +0530

    added interfaces for API data- defines the structure of response data

---
+++ src/types/MovieSummary.ts
@@ -0,0 +1,16 @@
+export interface MovieSummary {
+    imdbID: string;
+    Title: string;
+    Year: string;
+    Poster: string;
+}
+
+export interface MovieDetail {
+    Title: string;
+    Year: string;
+    Genre: string;
+    Plot: string;
+    Poster: string;
+    Director: string;
+    Actors: string;
+  }
